f1cd73ed1794fa171ff44cd7ae74cb66f953d0b1
akinori-matsumoto <73481750+akinoringo@users.noreply.github.com>
2022-05-28 10:50:40 +0900

    feat: ルートの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme/theme";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./router/Router";
 
 export default function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button colorScheme="teal">ボタン</Button>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
